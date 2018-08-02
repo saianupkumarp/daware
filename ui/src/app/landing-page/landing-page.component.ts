94f5b8430fb9d300200bb8cb061996912f30bf49
linahhamdan <linah.hamdan@kapsarc.org>
2018-08-03 02:45:06 +0300

    info window

--- ui/src/app/landing-page/landing-page.component.ts
+++ ui/src/app/landing-page/landing-page.component.ts
@@ -12,7 +12,20 @@ import 'rxjs/add/observable/interval';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  marker = {
+    display: true,
+    lat: null,
+    lng: null,
+  };
+  ind={
+    id: true,
+    nationality: null,
+    age: null,
+    passno:null,
+    bp:null,
+    hr:null
 
+  }
   sub;
   riskDataPoints = [];
   payloadBody;
@@ -71,8 +84,19 @@ export class LandingPageComponent implements OnInit {
     });
   }
 
-  showInfoWindow(ind_num) {
-    console.log(this.risk_popup_data[ind_num])
+  showInfoWindow({target: marker}, ind_num) {
+    console.log(this.risk_popup_data[ind_num]);
+    // this.marker.lat = marker.getPosition().lat();
+    // this.marker.lng = marker.getPosition().lng();
+    this.ind.id=this.risk_popup_data[ind_num][0];
+    this.ind.nationality=this.risk_popup_data[ind_num][1];
+    this.ind.age=this.risk_popup_data[ind_num][2];
+    this.ind.passno=this.risk_popup_data[ind_num][3];
+    this.ind.bp=this.risk_popup_data[ind_num][4];
+    this.ind.hr=this.risk_popup_data[ind_num][5];
+
+    marker.nguiMapComponent.openInfoWindow('iw', marker);
+    // this.infowindow.open(map,ind_num);
   }
 
 }

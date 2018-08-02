5ea3142c8b4907d5985619db9fdad63baf7ce5dd
linahhamdan <linah.hamdan@kapsarc.org>
2018-08-02 20:09:08 +0300

    light-bootstrap-dashboard-angular2r

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,24 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { LbdModule } from './lbd/lbd.module';
 
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { GmapComponent } from './gmap/gmap.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {SuiModule} from 'ng2-semantic-ui';
+
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { TablesComponent } from './tables/tables.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    GmapComponent,
-    DashboardComponent
+    HomeComponent,
+    UserComponent,
+    TablesComponent,
+    TypographyComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+    UpgradeComponent
+
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    NavbarModule,
+    FooterModule,
+    SidebarModule,
+    RouterModule,
     AppRoutingModule,
-    SuiModule
+    LbdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
